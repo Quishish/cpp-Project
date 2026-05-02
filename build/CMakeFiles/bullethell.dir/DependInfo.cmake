f9a9e3c6701282a456b24add077758d59937f472
MichaelStruczewski <mikhail.struczewski@icloud.com>
2026-05-02 17:28:24 +0300

    Added shooting enemies

---
+++ build/CMakeFiles/bullethell.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/cpp-Project/Bullet.cpp" "CMakeFiles/bullethell.dir/Bullet.cpp.o" "gcc" "CMakeFiles/bullethell.dir/Bullet.cpp.o.d"
+  "/mnt/d/cpp-Project/Enemy.cpp" "CMakeFiles/bullethell.dir/Enemy.cpp.o" "gcc" "CMakeFiles/bullethell.dir/Enemy.cpp.o.d"
+  "/mnt/d/cpp-Project/Entity.cpp" "CMakeFiles/bullethell.dir/Entity.cpp.o" "gcc" "CMakeFiles/bullethell.dir/Entity.cpp.o.d"
+  "/mnt/d/cpp-Project/Player.cpp" "CMakeFiles/bullethell.dir/Player.cpp.o" "gcc" "CMakeFiles/bullethell.dir/Player.cpp.o.d"
+  "/mnt/d/cpp-Project/ShooterEnemy.cpp" "CMakeFiles/bullethell.dir/ShooterEnemy.cpp.o" "gcc" "CMakeFiles/bullethell.dir/ShooterEnemy.cpp.o.d"
+  "/mnt/d/cpp-Project/main.cpp" "CMakeFiles/bullethell.dir/main.cpp.o" "gcc" "CMakeFiles/bullethell.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
